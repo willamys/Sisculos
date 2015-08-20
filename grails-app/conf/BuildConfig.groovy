2eb1b6ea0a3dee4d958239134ffdc1e0d3bc2a9e
willamys <willamysaraujo7@gmail.com>
2015-08-20 09:43:25 -0300

    Plugin heroku e cloud

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,6 +68,8 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 		runtime ':jasper:1.11.0'
+		compile ':heroku:1.0.1'
+		compile ':cloud-support:1.0.11'
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
